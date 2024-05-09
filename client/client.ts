7cd075205f0238812617eac4affe77b0192af7ae
Sandhya Govindaraju <sandhya.govindaraju@gmail.com>
2024-05-09 11:16:37 -0500

    Add feathers client

---
+++ client/client.ts
@@ -0,0 +1,20 @@
+import { feathers } from '@feathersjs/feathers'
+import rest from '@feathersjs/rest-client'
+import authentication from '@feathersjs/authentication-client'
+
+import { SERVER_URL } from './constants'
+
+const app = feathers()
+
+// Connect to the same as the browser URL (only in the browser)
+const restClient = rest(SERVER_URL)
+
+// Configure an AJAX library (see below) with that client
+app.configure(restClient.fetch(window.fetch.bind(window)))
+app.configure(authentication())
+// TODO: When logout, do app.logout()
+
+const usersService = app.service('users');
+// export const testsService = app.service('tests');
+
+export { app, usersService }
