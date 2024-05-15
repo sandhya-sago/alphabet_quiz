8c3b573dc09622007ae8bf3453bdcb1af3fdef9d
Sandhya Govindaraju <sandhya.govindaraju@gmail.com>
2024-05-15 14:10:01 -0500

    DEV: remove unused code

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,67 +1,27 @@
 import {
   Heading,
-  Text,
   Button,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
   Checkbox,
-  CheckboxGroup,
   Input,
   InputRightAddon,
   Stack,
   InputGroup,
-  keyframes,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { app, homeService } from "../client";
-import restClient from "@feathersjs/rest-client";
-import { SERVER_URL } from "../constants";
+import { homeService } from "../client";
 import { homeSchema } from "../../server/src/services/home/home.schema";
 
-function toTitleCase(str) {
+function toTitleCase(str: string) {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
 
 export const Home = () => {
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
   const [allTopics, setAllTopics] = useState<homeSchema[]>([]);
 
-  const selectFile = (event) => {
-    const fileElem = document.getElementById("fileElem");
-    if (fileElem) {
-      fileElem.click();
-    }
-    event.preventDefault();
-  };
-
-  const uploadFile = async (event) => {
-    console.log("File uploaded:", event.target, event.target.files[0]);
-    const data = new FormData();
-    data.append("files", event.target.files);
-    data.append("fileType", "FACTS_CSV");
-    data.append("text", "FACTS_CSV");
-    const { accessToken } = await app.get("authentication");
-    // https://github.com/feathersjs/feathers/issues/1744#issuecomment-568015824
-    fetch(`${SERVER_URL}/${FILE_UPLOAD}`, {
-      body: data,
-      method: "post",
-      headers: { Authorization: `Bearer ${accessToken}` },
-    })
-      .then((request) => request.text())
-      .then((response) => {
-        // success
-        console.log("Server responded with: ", response);
-      })
-      .catch((err) => console.log("Server error:", err))
-      .finally(() => console.log("finished"));
-    // Reset the input files, so that same file can be uploaded again
-    event.target.value = null;
-  };
   useEffect(() => {
     homeService.find().then((data) => {
       const topics = data
@@ -84,20 +44,6 @@ export const Home = () => {
     };
     console.log(data);
   };
-  // return (
-  //   <>
-  //     <input
-  //       type="file"
-  //       onChange={uploadFile}
-  //       id="fileElem"
-  //       accept=".csv"
-  //       style={{ display: "none" }}
-  //     />
-  //     <Button id="fileSelect" onClick={selectFile}>
-  //       Upload File
-  //     </Button>
-  //   </>
-  // );
   return (
     <>
       <Heading>Setup Test</Heading>
