e757ad0f22d019030dfd00641360abdde4ebcc2b
Sandhya Govindaraju <sandhya.govindaraju@gmail.com>
2024-05-20 23:30:30 -0500

    FIX: Add quiz to backend

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,9 +1,11 @@
+import { quiz } from './quiz/quiz'
 import { home } from './home/home'
 import { user } from './users/users'
 // For more information about this file see https://dove.feathersjs.com/guides/cli/application.html#configure-functions
 import type { Application } from '../declarations'
 
 export const services = (app: Application) => {
+  app.configure(quiz)
   app.configure(home)
   app.configure(user)
   // All services will be registered here
